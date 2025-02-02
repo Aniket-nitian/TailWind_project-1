ee38489a6280f30209b047d11a3284cd82393d0a
Aniket-nitian <anik807621@gmail.com>
2025-02-02 12:15:28 +0530

    Nav Bar completd

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3238f2",
+        aniket: "#01ee91",
+      },
+
+      fontFamily: {
+        font: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
